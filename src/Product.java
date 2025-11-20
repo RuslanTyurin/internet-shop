23276739dde9ebb650f284d116871f1e94c4ce01
RTyurin <rtyurinsocial@mail.ru>
2025-11-20 20:22:45 +0700

    5.3

---
+++ src/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price){
+        setName(name);
+        setPrice(price);
+    }
+
+    public void setName(String name){
+       this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public  void  setPrice(int price){
+        this.price = price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+}
